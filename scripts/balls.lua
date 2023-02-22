87975c4ee7bfb0bb25dafedc98d68b21e6834035
Darkceius <74603733+darkceius@users.noreply.github.com>
2023-02-22 12:46:34 +0200

    b

--- scripts/balls.lua
+++ scripts/balls.lua
@@ -1,74 +1,46 @@
 -- change to the rgb of ur shirt or torso if naked
 
---local shirtColor = Color3.fromRGB(1,1,1)
+local shirtColor = Color3.fromRGB(1,1,1)
 
 -- shutcho ass up
 
 local char = owner.Character
-
 local torso = char:FindFirstChild("Torso") or char:FindFirstChild("UpperTorso")
-local shirtColor = torso.Color
-local moving = true
+local moving = true 
 
 function newBall(mainCF)
+local ball = Instance.new("Part")
+ball.Size = Vector3.new(1,1,1)*1.1
+ball.CanCollide = false
+ball.Shape = "Ball"
+ball.Color = shirtColor
+ball.Material = "Plastic"
 
-   local ball = Instance.new("Part")
+local weld = Instance.new("Motor6D")
+weld.Parent = ball
+weld.Part0 = ball
+weld.Part1 = torso
+weld.C0 = mainCF
 
-   ball.Size = Vector3.new(1,1,1)*1.1
+ball.Parent = torso
 
-   ball.CanCollide = false
-
-   ball.Shape = "Ball"
-
-   ball.Color = shirtColor
-
-   ball.Material = "Plastic"
-
-   local weld = Instance.new("Motor6D")
-
-   weld.Parent = ball
-
-   weld.Part0 = ball
-
-   weld.Part1 = torso
-
-   weld.C0 = mainCF
-
-   ball.Parent = torso
-
-   task.spawn(function()
-
-   while wait() do
-
-      if not moving then
-
-         weld.C0 = weld.C0:Lerp(mainCF,.2)
-         continue
-      end
-
-      local sin = math.sin((tick()*4)%(math.pi*2))
-
-      weld.C0 = weld.C0:Lerp(mainCF * CFrame.new(0, sin/7, sin/14) * CFrame.Angles(math.clamp(sin, -2, 0.4)/10,0,0),0.2)
-
-   end
-
-   end)
-
+task.spawn(function()
+while wait() do
+if not moving then
+weld.C0 = weld.C0:Lerp(mainCF,.2)
+continue
+end
+local sin = math.sin((tick()*4)%(math.pi*2))
+weld.C0 = weld.C0:Lerp(mainCF * CFrame.new(0, sin/7, sin/17),0.2) --* CFrame.Angles(math.clamp(sin, -1, 1)/10,0,0),0.2)
+end
+end)
 end
 
 task.spawn(function()
-
 while wait() do
-
-   moving = char.Humanoid.MoveDirection.Magnitude > 0
-
+moving = char.Humanoid.MoveDirection.Magnitude > 0
 end
-
 end)
-
-local up = torso.Name == "Torso" and -.2 or -.1
-
+local up = torso.Name == "Torso" and -.25 or -.1
 newBall(CFrame.new(.4,up,.22))
-
 newBall(CFrame.new(-.4,up,.22))
-
